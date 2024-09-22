a5ea2093e37d070a295bc48c0e879c0a6a632678
Karla Jiménez Millar <kjmillar0@gmail.com>
2024-09-22 14:47:24 -0300

    ADD Operadores Aritmeticos

---
+++ Operadores Aritmeticos/script.js
@@ -0,0 +1,22 @@
+const numero1 = 20;
+const numero2 = 40;
+
+//Ejercicio 1
+let suma = numero1 + numero2;
+console.log("El resultado de la suma es: " + suma);
+
+//Ejercicio 2
+let resta = numero1 - numero2;
+console.log("El resultado de la resta es: " + resta);
+
+//Ejercicio 3
+let multiplicacion = numero1 * numero2;
+console.log("El resultado de la multiplicación es: " + multiplicacion);
+
+//Ejercicio 4
+let division = numero1 / numero2;
+console.log("El resultado de la división es: " + division);
+
+//Ejercicio 5
+let modulo = numero2 % numero1;
+console.log("El resultado del resto en la división entre 40 y 20 es: " + modulo);
