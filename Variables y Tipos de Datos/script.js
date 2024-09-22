01dab9994df6f1da55f8d76e2660f5b27edfe3bc
Karla Jiménez Millar <kjmillar0@gmail.com>
2024-09-22 14:31:03 -0300

    ADD Variables y Tipos de Datos

---
+++ Variables y Tipos de Datos/script.js
@@ -0,0 +1,18 @@
+//Ejercicio 1
+let nombre = "Karla";
+
+//Ejercicio 2
+let edad = 25;
+
+//Ejercicio 3
+let puedoConducir = false;
+
+//Ejercicio 4
+console.log(nombre);
+console.log(edad);
+console.log(puedoConducir);
+
+//Ejercicio 5
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof puedoConducir);
