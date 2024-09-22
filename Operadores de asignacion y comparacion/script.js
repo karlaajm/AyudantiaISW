ad368eb02a39adef697ed08e2ed97b0a292a1ebf
Karla Jiménez Millar <kjmillar0@gmail.com>
2024-09-22 15:01:34 -0300

    ADD Operadores de asignacion y comparacion

---
+++ Operadores de asignacion y comparacion/script.js
@@ -0,0 +1,21 @@
+const numero1 = 15;
+const numero2 = 20;
+const numero3 = '25';
+
+//Ejercicio 1
+console.log(numero1 >= numero2);
+
+//Ejercicio 2
+console.log(numero1 <= numero2);
+
+//Ejercicio 3
+console.log(numero1 < numero3);
+
+//Ejercicio 4
+console.log(numero3 < numero2);
+
+//Ejercicio 5
+console.log(numero3 !== numero1);
+
+//Ejercicio 6
+console.log(numero1 === numero2);
