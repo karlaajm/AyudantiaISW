3df0ddea5879dc831155e656c9b7e49cbae47f13
Karla Jiménez Millar <kjmillar0@gmail.com>
2024-09-26 12:59:30 -0300

    ADD Funciones

---
+++ Funciones/script.js
@@ -0,0 +1,47 @@
+//Ejericico 1
+function mayusculas (palabras) {
+    return palabras.toUpperCase();
+}
+
+let nombreYApellido = "karla jimenez";
+console.log(mayusculas(nombreYApellido));
+
+//Ejericico 2
+function minusculas (frase) {
+    return frase.toLowerCase();
+}
+
+let oracion = "Hola soy Karla";
+console.log(minusculas(oracion));
+
+//Ejericico 3
+function resta (numero1, numero2) {
+    return numero2 - numero1;
+}
+
+let resultadoResta = resta(20, 40);
+console.log(resultadoResta);
+
+//Ejericico 4
+function division (numero1, numero2) {
+    return numero2 / numero1;
+}
+
+let resultadoDivision = division(20, 40);
+console.log(resultadoDivision);
+
+//Ejericico 5
+function multiplicacion (numero1, numero2) {
+    return numero2 * numero1;
+}
+
+let resultadoMultiplicacion = multiplicacion(20, 40);
+console.log(resultadoMultiplicacion);
+
+//Ejericico 6
+function longitud (palabra) {
+    return palabra.length;
+}
+
+let expresion = "Felicidad";
+console.log(longitud(expresion));
