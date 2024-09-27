6e467771c3e6f906fbc15ffa2618f34bbe6eb48d
Karla Jiménez Millar <kjmillar0@gmail.com>
2024-09-26 23:07:46 -0300

    ADD Objetos

---
+++ Objetos/script.js
@@ -0,0 +1,24 @@
+//Ejercicio 1
+const persona = {
+    nombre: "Karla Jiménez Millar",
+    edad: 25,
+    genero: "Femenino"
+}
+
+console.log(persona);
+
+//Ejercicio 2
+const caja = {
+    cuadernos: "buen estado",
+    lapices: "mal estado",
+    fotografias: "buen estado"
+}
+
+console.log(caja);
+console.log(caja.cuadernos);
+console.log(caja.lapices);
+console.log(caja.fotografias);
+console.log(typeof caja);
+console.log(typeof caja.cuadernos);
+console.log(typeof caja.lapices);
+console.log(typeof caja.fotografias);
